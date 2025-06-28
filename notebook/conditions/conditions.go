4fbf7456919f0c819e4def168e16c4c9a2d6298c
joaomarcosg <joaomarcos.ipms@gmail.com>
2025-06-28 20:54:18 -0300

    FEAT: adiciona exemplo de condicional or

--- notebook/conditions/conditions.go
+++ notebook/conditions/conditions.go
@@ -1,1 +1,19 @@
 package conditions
+
+import "fmt"
+
+const color = "Blue"
+const year = 2017
+
+func CheckOrConditions() {
+
+	// parênteses indica ordem de precedência das comparações
+	if (color == "Blue" || color == "White") && year == 2017 {
+		fmt.Println("primeiro compara Blue or White")
+	}
+
+	if color == "Blue" && (year == 2016 || year == 2017) {
+		fmt.Println("primeiro compara year 2016 or year 2017")
+	}
+
+}
